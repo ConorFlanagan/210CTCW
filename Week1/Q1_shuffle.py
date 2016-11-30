987bdb766494b9ad1a1df896d278ef4dce66efd5
Conor Flanagan <conor.flanagan24@gmail.com>
2016-11-30 20:10:53 +0000

    Added comments

--- Week1/Q1_shuffle.py
+++ Week1/Q1_shuffle.py
@@ -1,13 +1,14 @@
 import random
 
 def shuf(List):
-    for a in range(len(List)-1,0,-1):
-        b=random.randint(0,a)
+    
+    for a in range(len(List)-1,0,-1):       #For each index of array
+        b=random.randint(0,a)               #Selects random second index
         if b == a:
             continue
-        List[a],List[b] = List[b],List[a]
+        List[a],List[b] = List[b],List[a]   #Swap selected indexes
+        
     return (List)
 
 #Run-time bounds (Big O):
-#Total = 5n + 2
 #O(n)
