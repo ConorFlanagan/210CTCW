fdf92b1526cae8dbaebb669b77094f60645fded9
ConorFlanagan <ConorFlanagan@users.noreply.github.com>
2016-12-02 20:20:51 +0000

    Update Q2_factorialZeros.py
    Comments

--- Week1/Q2_factorialZeros.py
+++ Week1/Q2_factorialZeros.py
@@ -4,11 +4,11 @@ def factZeros(a):
     trail = 0
     for n in range(5, a+1):
         
-        while n:
+        while n:                #While divisible by 5, the loop will run
             
-            if n%5 == 0:
+            if n%5 == 0:        
                 trail += 1
-                n = n/5
+                n = n/5         #New value of n if divisible by 5 with no remainder
                 
             else:
                 break
